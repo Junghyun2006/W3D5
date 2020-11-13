95968ae303cc3677eb2b80ef20c6c61a7c6f3b3a
junghyun2006 <jung.hyun.park516@gmail.com>
2020-11-13 15:47:47 -0500

    Add dfs

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -23,5 +23,25 @@ class PolyTreeNode
         end
     end
 
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        # raise "node is not a child" if !self.children.include?(child_node)
+        # child_node.parent = nil
+        self.children.include?(child_node) ? (child_node.parent = nil) : (raise "node is not a child") 
+    end
+
+    def dfs(target_value)
+        return nil if children.empty?
+        return self if value == target_value
+
+        children.each_with_index do [child,i]
+            # return dfs(children[i]) if children[i]
+            child.dfs(target_value)
+        end
+    end
+
 end
 
